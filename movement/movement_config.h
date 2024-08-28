2faa4e0bb1395f4278198639542a329892b66194
Zack <zphixon@gmail.com>
2024-08-28 13:37:38 -0400

    my config

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -28,15 +28,20 @@
 #include "movement_faces.h"
 
 const watch_face_t watch_faces[] = {
-    simple_clock_face,
-    world_clock_face,
-    sunrise_sunset_face,
+    clock_face,
+    beats_face,
     moon_phase_face,
+    astronomy_face,
     stopwatch_face,
+    timer_face,
+    pulsometer_face,
+    probability_face,
+    rpn_calculator_face,
+    invaders_face,
+    voltage_face, // 10
     preferences_face,
     set_time_face,
-    thermistor_readout_face,
-    voltage_face
+    fsl_face
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -47,7 +52,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2) // or (0)
+#define MOVEMENT_SECONDARY_FACE_INDEX 10
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
@@ -62,7 +67,7 @@ const watch_face_t watch_faces[] = {
 #define MOVEMENT_DEFAULT_24H_MODE false
 
 /* Enable or disable the sound on mode button press */
-#define MOVEMENT_DEFAULT_BUTTON_SOUND true
+#define MOVEMENT_DEFAULT_BUTTON_SOUND false
 
 /* Set the timeout before switching back to the main watch face
  * Valid values are:
